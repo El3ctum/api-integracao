af83871d789e382f85059d68d59147549db4cc75
Davi Leal Marçal <53124599+El3ctum@users.noreply.github.com>
2025-10-15 17:52:40 -0300

    Clean up Service interface
    Removed comments from the Service interface in database.go.

--- internal/database/database.go
+++ internal/database/database.go
@@ -17,16 +17,11 @@ func init() {
 	}
 }
 
-// Service represents a service that interacts with a database
 type Service interface {
-	// Health returns a map of health status information.
-	// The keys and values in the map are service-specific.
 	Health() map[string]string
 
 	GetScope() *gocb.Scope
 
-	// Close terminates the database connection.
-	// It returns an error if the connection cannot be closed.
 	Close() error
 }
 
